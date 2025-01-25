e48196984a168d858c039a35ab2fe822aa2ee214
vaibhavdwivedi2710 <vaibhavdwivedi2710@gmail.com>
2025-01-25 23:05:41 +0530

    Added Logging in comment APIs

--- src/main/java/com/blogging_app/controller/CommentController.java
+++ src/main/java/com/blogging_app/controller/CommentController.java
@@ -3,6 +3,8 @@ package com.blogging_app.controller;
 import com.blogging_app.dto.ApiResponse;
 import com.blogging_app.dto.CommentDto;
 import com.blogging_app.service.CommentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,23 +14,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/blog/api/")
 public class CommentController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
+
     @Autowired
     private CommentService commentService;
 
     @PostMapping("/post/{postId}/comment")
-    public ResponseEntity<CommentDto>createComment(
-            @RequestBody CommentDto commentDto,
-            @PathVariable Integer postId )
-    {
-     CommentDto createComment = this.commentService.createComment(commentDto,postId);
-     return new ResponseEntity<CommentDto>(createComment, HttpStatus.CREATED);
+    public ResponseEntity<CommentDto>createComment( @RequestBody CommentDto commentDto, @PathVariable Integer postId ) {
+
+        logger.info("Received request to create a comment for post ID: {} with request body: {}", postId, commentDto);
+        CommentDto createComment = this.commentService.createComment(commentDto,postId);
+        logger.info("Comment created successfully for post ID: {} with response: {}", postId, createComment);
+        return new ResponseEntity<CommentDto>(createComment, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/comments/delete/{commentId}")
-    public ResponseEntity<ApiResponse>deleteComment(
-            @PathVariable Integer commentId )
-    {
+    public ResponseEntity<ApiResponse>deleteComment( @PathVariable Integer commentId ) {
+
+        logger.info("Received request to delete comment with ID: {}", commentId);
         this.commentService.deleteComment(commentId);
+        logger.info("Comment deleted successfully with ID: {}", commentId);
         return new ResponseEntity<ApiResponse>(new ApiResponse("Comment Deleted Successfully",true),HttpStatus.OK);
     }
 
--- src/main/java/com/blogging_app/service/impl/CommentServiceImpl.java
+++ src/main/java/com/blogging_app/service/impl/CommentServiceImpl.java
@@ -9,11 +9,15 @@ import com.blogging_app.repository.CommentRepository;
 import com.blogging_app.repository.PostRepository;
 import com.blogging_app.service.CommentService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CommentServiceImpl.class);
 
     @Autowired
     private PostRepository postRepository;
@@ -26,13 +30,16 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public CommentDto createComment(CommentDto commentDto, Integer postId) {
+        logger.debug("Processing request to create a comment for post ID: {}", postId);
 
-        Post post = this.postRepository.findById(postId).orElseThrow(()-> new ResourceNotFoundException("Post","postId",postId));
+        Post post = this.postRepository.findById(postId)
+                .orElseThrow(() -> {
+                    logger.error("Post with ID {} not found", postId);
+                    return new ResourceNotFoundException("Post", "postId", postId);
+                });
 
         Comment comment = this.modelMapper.map(commentDto,Comment.class);
-
         comment.setPost(post);
-
         Comment savedComment = this.commentRepository.save(comment);
         return this.modelMapper.map(savedComment,CommentDto.class);
 
@@ -40,7 +47,14 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public void deleteComment(Integer commentId) {
-        Comment comment = this.commentRepository.findById(commentId).orElseThrow(()-> new ResourceNotFoundException("Comment","commentId",commentId));
+        logger.debug("Processing request to delete comment with ID: {}", commentId);
+
+        // Fetch comment by ID
+        Comment comment = this.commentRepository.findById(commentId)
+                .orElseThrow(() -> {
+                    logger.error("Comment with ID {} not found", commentId);
+                    return new ResourceNotFoundException("Comment", "commentId", commentId);
+                });
         this.commentRepository.delete(comment);
     }
 }
